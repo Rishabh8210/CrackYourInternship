867f37144d46c267730421cfb8019251c2470ca2
Rishabh Pandey <rishabhpandey8092@gmail.com>
2024-07-29 23:35:01 +0530

    String hard ques

---
+++ String/3. Hard/Word Wrap.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int square(int n) {
+    return n * n;
+}
+
+int solveWordWrapUtil(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>>& memo) {
+    if (wordIndex == n - 1) {
+        memo[wordIndex][remLength] = words[wordIndex] < remLength ? 0 : square(remLength);
+        return memo[wordIndex][remLength];
+    }
+
+    int currWord = words[wordIndex];
+    if (currWord < remLength) {
+        return min(
+            solveWordWrapUsingMemo(words, n, length, wordIndex + 1, remLength == length ? remLength - currWord : remLength - currWord - 1, memo),
+            square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo)
+        );
+    } else {
+        return square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo);
+    }
+}
+
+int solveWordWrapUsingMemo(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>>& memo) {
+    if (memo[wordIndex][remLength] != -1) {
+        return memo[wordIndex][remLength];
+    }
+
+    memo[wordIndex][remLength] = solveWordWrapUtil(words, n, length, wordIndex, remLength, memo);
+    return memo[wordIndex][remLength];
+}
+
+int solveWordWrap(int words[], int n, int k) {
+    vector<vector<int>> memo(n, vector<int>(k + 1, -1));
+    return solveWordWrapUsingMemo(words, n, k, 0, k, memo);
+}
